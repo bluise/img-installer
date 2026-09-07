46218cc52df51d31935b12b4f5881cc56722b5d0
bluise <104455614+bluise@users.noreply.github.com>
2026-09-07 09:37:55 +0800

    Create istoreos25.sh

---
+++ istoreos25.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+FIRMWARE_URL="https://fw0.koolcenter.com/iStoreOS/alpha/25.12/x86_64_efi/istoreos-25.12.5-2026082723-x86-64-squashfs-combined-efi.img.gz"
+FIRMWARE_ARCHIVE="gz"
+FIRMWARE_NAME="iStoreOS‑25.12"
+source ./autobuild/build-main.sh
