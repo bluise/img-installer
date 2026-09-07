a8ad61d9c69f5b294a85d68da4fe5ca9ed97657f
bluise <104455614+bluise@users.noreply.github.com>
2026-09-07 10:01:47 +0800

    iStoreOS-25.12

--- istoreos25.sh
+++ istoreos25.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-FIRMWARE_URL="https://fw0.koolcenter.com/iStoreOS/alpha/25.12/x86_64-efi/iStoreOS-25.12.5-2026082723-x86-64-squashfs-combined-efi.img.gz"
+FIRMWARE_URL="https://fw0.koolcenter.com/iStoreOS/alpha/25.12/x86_64_efi/istoreos-25.12.5-2026082723-x86-64-squashfs-combined-efi.img.gz"
 FIRMWARE_ARCHIVE="gz"
 FIRMWARE_NAME="iStoreOS-25.12"
 chmod +x ./autobuild/autobuild.sh
